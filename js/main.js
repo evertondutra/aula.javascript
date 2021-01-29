ea8ab25c99c8e14a32ba42ec6bd383dfecce1073
Everton Dutra <evertondutra02@gmail.com>
2021-01-29 11:41:01 -0300

    Arrays e dicionários

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var nome = "Everton Dutra";
+/*var nome = "Everton Dutra";
 var idade = 29
 var idade2 = 10
 var frase = "Japão é o melhor time do mundo";
@@ -7,3 +7,17 @@ var frase = "Japão é o melhor time do mundo";
 console.log(nome);
 console.log(idade + idade2);
 console.log(frase.replace("Japão", "Brasil"));
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "))
+//alert(lista[2]);
+
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);
+*/
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas[0].cor);
